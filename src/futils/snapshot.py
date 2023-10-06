51d222c226f4746bf232968e22889f815b80a5eb
Francesco Terenzi <fra.terenz1993@gmail.com>
2023-10-06 16:53:59 +0100

    cdfs

--- src/futils/snapshot.py
+++ src/futils/snapshot.py
@@ -4,11 +4,70 @@ The histogram are stored as json files with keys being a quantity of interest
 and the values are the number of individuals.
 """
 import numpy as np
-from typing import Dict, List, NewType
+import json
+from typing import Dict, List, NewType, Tuple
+from pathlib import Path
 
 
 Distribution = NewType("Distribution", Dict[int, float])
 Histogram = NewType("Histogram", Dict[int, int])
+
+
+def histogram_from_file(file: Path) -> Histogram:
+    with open(file, "r") as f:
+        return Histogram({int(x): int(y) for x, y in json.load(f).items()})
+
+
+def cdf_from_histogram(hist: Histogram) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    >>> from src.futils import snapshot
+    >>> my_keys = [1, 0, 3]
+    >>> my_values = [2, 2, 1]
+    >>> histogram = snapshot.Histogram({k: ele for k, ele in zip(my_keys, my_values)})
+    >>> cdf = snapshot.cdf_from_histogram(histogram)
+    >>> cdf[0]
+    array([0, 1, 3])
+    >>> cdf[1]
+    array([0.4, 0.8, 1. ])
+    """
+    distr = distribution_from_histogram(hist)
+    return cdf_from_distribution(distr)
+
+
+def cdf_from_distribution(distr: Distribution) -> Tuple[np.ndarray, np.ndarray]:
+    """Return the x-axis (all the possible states) and the cumulative
+    distribution (y-axis)
+    >>> from src.futils import snapshot
+    >>> my_keys = [1, 0, 3]
+    >>> my_values = [2, 2, 1]
+    >>> distr = snapshot.Distribution({k: ele/5 for k, ele in zip(my_keys, my_values)})
+    >>> cdf = snapshot.cdf_from_distribution(distr)
+    >>> cdf[0]
+    array([0, 1, 3])
+    >>> cdf[1]
+    array([0.4, 0.8, 1. ])
+    """
+    ordered_distr = dict(sorted(distr.items()))
+    return np.array(list(ordered_distr.keys()), dtype=int), np.array(
+        np.cumsum(list(ordered_distr.values()), dtype=float), dtype=float
+    )
+
+
+def distribution_from_histogram(hist: Histogram) -> Distribution:
+    """
+    >>> from src.futils import snapshot
+    >>> my_keys = [0, 1, 3]
+    >>> my_values = [2, 2, 1]
+    >>> histogram = snapshot.Histogram({k: ele for k, ele in zip(my_keys, my_values)})
+    >>> distribution = snapshot.distribution_from_histogram(histogram)
+    >>> assert list(distribution.keys()) == my_keys
+    >>> list(distribution.keys())
+    [0, 1, 3]
+    >>> [round(ele, 2) for ele in distribution.values()]
+    [0.4, 0.4, 0.2]
+    """
+    ntot = sum(hist.values())
+    return Distribution({k: ele / ntot for k, ele in hist.items()})
 
 
 class HistogramsUniformised:
